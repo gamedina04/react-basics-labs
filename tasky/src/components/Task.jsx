bf76ccb8222186435ac0d5f35c25fae0bbe454dd
Guido Medina <guido.a.medina@gmail.com>
2026-09-16 10:30:43 +0100

    Tasky Lab 2a - Exercise done

--- tasky/src/components/Task.jsx
+++ tasky/src/components/Task.jsx
@@ -4,6 +4,7 @@ const Task = (props) => {
         <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
+            <h3>Priority: {props.priority}</h3>
             <p className="description">{props.description}</p>
         </div>
     )
