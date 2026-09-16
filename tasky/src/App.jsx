ee3ca87bd30dbd970beede03342136f6cac57fec
Guido Medina <guido.a.medina@gmail.com>
2026-09-16 10:58:47 +0100

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
 import AddTaskForm from './components/Form';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [ taskState, setTaskState ] = useState({
@@ -10,6 +11,11 @@ function App() {
       { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "Medium", done: false },
       { id: 3, title: "Tidy up", deadline: "Today", priority: "Low", done: false }
     ]
+  });
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
   });
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
@@ -21,6 +27,37 @@ function App() {
     tasks.splice(taskIndex, 1);
     setTaskState({tasks});
   } 
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+    console.log(formState);
+  }
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -36,10 +73,13 @@ function App() {
       deleteTask={() => deleteHandler(index)}
     />
   ))}
-  <AddTaskForm />
+  
+  <AddTaskForm change={formChangeHandler} submit={formSubmitHandler} />
+  
     </div>
   );
 }
+
 
 
 export default App;
